47fc4ee34924a23feefc33533cb0890d973e8671
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2021-09-15 00:24:48 -0300

    feat: Create Header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import PokeCard from "./poke-card/poke-card";
 import PokeTeam from "./poke-team/poke-team";
 import PokeTeamCard from "./poke-team-card/poke-team-card";
+import Header from "./header/header";
 
-export { PokeCard, PokeTeam, PokeTeamCard };
+export { PokeCard, PokeTeam, PokeTeamCard, Header };
