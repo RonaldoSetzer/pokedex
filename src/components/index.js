b98dbb38b5fe1708f1c5b1dbd10de4923e4fdc5d
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2021-09-15 00:29:45 -0300

    feat: Create Footer component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import PokeCard from "./poke-card/poke-card";
 import PokeTeam from "./poke-team/poke-team";
 import PokeTeamCard from "./poke-team-card/poke-team-card";
 import Header from "./header/header";
+import Footer from "./footer/footer";
 
-export { PokeCard, PokeTeam, PokeTeamCard, Header };
+export { PokeCard, PokeTeam, PokeTeamCard, Header, Footer };
