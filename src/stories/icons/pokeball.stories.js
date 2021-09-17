b9296229a2a2b925610728eb58f8250e9a2d3591
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2021-09-16 23:00:44 -0300

    feat: Add pokeball icon

---
+++ src/stories/icons/pokeball.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Pokeball } from "../../icons/";
+
+export default {
+  title: "Icons/Pokeball",
+  component: Pokeball,
+};
+
+const Template = (args) => <Pokeball {...args} />;
+
+export const Default = Template.bind({});
+
